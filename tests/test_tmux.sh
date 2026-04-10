12dd7df55ea4b1a5cdca8f9e79f39abcc9d73526
Everton Andrade <everton@andrade.mat.br>
2026-04-10 12:21:38 -0400

    feat: add tmux window and split shortcuts

--- tests/test_tmux.sh
+++ tests/test_tmux.sh
@@ -16,6 +16,9 @@ test_tmux_config_enables_modern_defaults() {
   assert_contains "$config" "setw -g mode-keys vi" "tmux copy mode should use vi keys"
   assert_contains "$config" "set -g prefix C-a" "tmux config should move the prefix to C-a"
   assert_contains "$config" "#{pane_current_path}" "new panes and windows should inherit the current path"
+  assert_contains "$config" "bind T new-window -c \"#{pane_current_path}\"" "tmux config should provide an explicit T shortcut for new windows"
+  assert_contains "$config" "bind -n PageDown split-window -h -c \"#{pane_current_path}\"" "tmux config should split side by side with PageDown"
+  assert_contains "$config" "bind -n PageUp split-window -v -c \"#{pane_current_path}\"" "tmux config should split top-bottom with PageUp"
   assert_contains "$config" "bind -r h select-pane -L" "tmux config should provide vim-style pane navigation"
   assert_contains "$config" "bind -r L resize-pane -R 5" "tmux config should provide pane resizing shortcuts"
 }
